6f1c6d2d95024effb4584da9b8630b97c9ae7b50
Nigel Feng <10083@holbertonstudents.com>
2025-01-13 16:16:09 +1100

    3. Debugging - Python Mines

--- debugging/mines.py
+++ debugging/mines.py
@@ -4,9 +4,11 @@ import random
 import os
 
 def clear_screen():
+    """clear screen"""
     os.system('cls' if os.name == 'nt' else 'clear')
 
 class Minesweeper:
+    """Minesweeper field"""
     def __init__(self, width=10, height=10, mines=10):
         self.width = width
         self.height = height
@@ -15,13 +17,14 @@ class Minesweeper:
         self.revealed = [[False for _ in range(width)] for _ in range(height)]
 
     def print_board(self, reveal=False):
+        """print board"""
         clear_screen()
         print('  ' + ' '.join(str(i) for i in range(self.width)))
         for y in range(self.height):
             print(y, end=' ')
             for x in range(self.width):
                 if reveal or self.revealed[y][x]:
-                    if (y * self.width + x) in self.mines:
+                    if y * self.width + x in self.mines:
                         print('*', end=' ')
                     else:
                         count = self.count_mines_nearby(x, y)
@@ -31,17 +34,19 @@ class Minesweeper:
             print()
 
     def count_mines_nearby(self, x, y):
+        """count mines nearby"""
         count = 0
         for dx in [-1, 0, 1]:
             for dy in [-1, 0, 1]:
                 nx, ny = x + dx, y + dy
                 if 0 <= nx < self.width and 0 <= ny < self.height:
-                    if (ny * self.width + nx) in self.mines:
+                    if ny * self.width + nx in self.mines:
                         count += 1
         return count
 
     def reveal(self, x, y):
-        if (y * self.width + x) in self.mines:
+        """reveal"""
+        if y * self.width + x in self.mines:
             return False
         self.revealed[y][x] = True
         if self.count_mines_nearby(x, y) == 0:
@@ -53,11 +58,15 @@ class Minesweeper:
         return True
 
     def play(self):
+        """play minesweep"""
         while True:
             self.print_board()
             try:
                 x = int(input("Enter x coordinate: "))
                 y = int(input("Enter y coordinate: "))
+                if not (0 <= x < self.width and 0 <= y < self.height):
+                    print("Invalid coordinates. Please try again.")
+                    continue
                 if not self.reveal(x, y):
                     self.print_board(reveal=True)
                     print("Game Over! You hit a mine.")
