2e7e4c1d279cbe0196210d567de800770cdac3fc
Nigel Feng <10083@holbertonstudents.com>
2025-01-13 12:38:02 +1100

    directory

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,10 +1,13 @@
+"""0. Debugging - Python Factorial"""
 #!/usr/bin/python3
 import sys
 
 def factorial(n):
+    """factorial of n"""
     result = 1
     while n > 1:
         result *= n
+        n = n - 1
     return result
 
 f = factorial(int(sys.argv[1]))
