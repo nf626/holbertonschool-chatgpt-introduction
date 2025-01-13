b45785fb4e3ab6718bd4e4b9e9985b22107a001e
Nigel Feng <10083@holbertonstudents.com>
2025-01-13 16:56:09 +1100

    5. Error Handling - Python Checkbook

---
+++ debugging/checkbook.py
@@ -0,0 +1,46 @@
+"""Checkbook"""
+class Checkbook:
+    """Checkbook class"""
+    # balance
+    def __init__(self):
+        self.balance = 0.0
+
+    def deposit(self, amount):
+        """deposit"""
+        self.balance += amount
+        print("Deposited ${:.2f}".format(amount))
+        print("Current Balance: ${:.2f}".format(self.balance))
+
+    def withdraw(self, amount):
+        """withdraw"""
+        if amount > self.balance:
+            print("Insufficient funds to complete the withdrawal.")
+        else:
+            self.balance -= amount
+            print("Withdrew ${:.2f}".format(amount))
+            print("Current Balance: ${:.2f}".format(self.balance))
+
+    def get_balance(self):
+        """get balance"""
+        print(f"{Current Balance: ${:.2f}}".format(self.balance))
+
+def main():
+    """main"""
+    cb = Checkbook()
+    while True:
+        action = input("What would you like to do? (deposit, withdraw, balance, exit): ")
+        if action.lower() == 'exit':
+            break
+        elif action.lower() == 'deposit':
+            amount = float(input("Enter the amount to deposit: $"))
+            cb.deposit(amount)
+        elif action.lower() == 'withdraw':
+            amount = float(input("Enter the amount to withdraw: $"))
+            cb.withdraw(amount)
+        elif action.lower() == 'balance':
+            cb.get_balance()
+        else:
+            print("Invalid command. Please try again.")
+
+if __name__ == "__main__":
+    main()
